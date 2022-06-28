f3040efa7183df013c21cd0d10c7be1e61881eac
axeliono <69375446+axeliono@users.noreply.github.com>
2022-06-27 20:34:52 -0500

    creating and running tests for mongodb models with jest

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const DATABASE_URL = process.env.DATABASE_URL;
-
+let mongo = undefined;
 const connectDB = () => {
   return mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/flashhaven',
@@ -8,16 +8,31 @@ const connectDB = () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
-    err => {
-        if(err) {
-            console.error('Connection to DB failed');
-        } else {
-            console.log('Connection to DB successful');
-        }
+    (err) => {
+      if (err) {
+        console.error('Connection to DB failed');
+      } else {
+        console.log('Connection to DB successful');
+      }
     }
   );
 };
 
+module.exports.dropCollections = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+};
+
+module.exports.dropDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  mongo.stop();
+};
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection failed'));
-module.exports = connectDB;
+module.exports = { connectDB, db };
